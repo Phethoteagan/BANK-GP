518e8c4a11e000612a052cff7b1c5c8a0402a906
Phethoteagan <79714067+Phethoteagan@users.noreply.github.com>
2021-05-30 23:46:23 +0200

    Add files via upload

--- gbv.sql
+++ gbv.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 29, 2021 at 10:55 PM
+-- Generation Time: May 30, 2021 at 11:35 PM
 -- Server version: 10.4.18-MariaDB
 -- PHP Version: 7.4.16
 
@@ -33,22 +33,27 @@ CREATE TABLE `contact` (
   `age` int(225) NOT NULL,
   `email` varchar(225) NOT NULL,
   `contactnum` varchar(225) NOT NULL,
-  `paddress` varchar(225) NOT NULL
+  `paddress` varchar(225) NOT NULL,
+  `comment` varchar(225) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `contact`
 --
 
-INSERT INTO `contact` (`id`, `fullName`, `age`, `email`, `contactnum`, `paddress`) VALUES
-(1, 'Gerald Macherechedze', 23, 'mobilegerald1@gmail.com', '0835912270', '10 Bentley place Dawn park'),
-(2, 'Teagan Tshikuvhe', 21, 'sililocom640@gmail.com', '0649044829', 'Bramfontain'),
-(3, 'Brendon Chiriga', 22, 'brendonchiriga@gmail.com', '0649044829', '24 First Street, UJ'),
-(4, 'Mathebula Ashley', 22, 'mathebula.ak@455gmail.com', '0649044829', '31 caroline street'),
-(5, 'Amos Tategulu', 23, 'amostatezz@gmail.com', '0649044829', 'home'),
-(7, 'helloworld', 100, 'world@gmail.com', '011011011', 'World'),
-(8, 'helloworld', 100, 'world@gmail.com', '011011011', 'world'),
-(9, 'helloworld', 383464648, 'world@gmail.com', '0649044829', 'world');
+INSERT INTO `contact` (`id`, `fullName`, `age`, `email`, `contactnum`, `paddress`, `comment`) VALUES
+(1, 'Gerald Macherechedze', 23, 'mobilegerald1@gmail.com', '0835912270', '10 Bentley place Dawn park', 'Hellow'),
+(2, 'Teagan Tshikuvhe', 21, 'sililocom640@gmail.com', '0649044829', 'Bramfontain', 'rape'),
+(3, 'Brendon Chiriga', 22, 'brendonchiriga@gmail.com', '0649044829', '24 First Street, UJ', ''),
+(4, 'Mathebula Ashley', 22, 'mathebula.ak@455gmail.com', '0649044829', '31 caroline street', ''),
+(5, 'Amos Tategulu', 23, 'amostatezz@gmail.com', '0649044829', 'home', ''),
+(7, 'helloworld', 100, 'world@gmail.com', '011011011', 'World', ''),
+(8, 'helloworld', 100, 'world@gmail.com', '011011011', 'world', ''),
+(9, 'helloworld', 383464648, 'world@gmail.com', '0649044829', 'world', ''),
+(11, 'gerald', 12, '10101@gmail.com', '0835912270', '10101', 'Assult'),
+(12, 'gerald', 12, '10101@gmail.com', '0835912270', '10101', 'Assult'),
+(13, 'gerald', 12, '10101@gmail.com', '0835912270', '10101', 'Assult'),
+(14, 'hjjsd', 0, 'sajs@gmail.com', 'cnds', 'dsd', 'sdd');
 
 -- --------------------------------------------------------
 
@@ -94,7 +99,7 @@ ALTER TABLE `login`
 -- AUTO_INCREMENT for table `contact`
 --
 ALTER TABLE `contact`
-  MODIFY `id` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `login`
